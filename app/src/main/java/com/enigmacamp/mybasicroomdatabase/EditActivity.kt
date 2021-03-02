9e53a840050147745082ad80129e56b9b44b1fdb
doni <donioctavian1016@gmail.com>
2021-03-03 00:55:34 +0700

    show_discribe_byid

--- app/src/main/java/com/enigmacamp/mybasicroomdatabase/EditActivity.kt
+++ app/src/main/java/com/enigmacamp/mybasicroomdatabase/EditActivity.kt
@@ -14,39 +14,53 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class EditActivity : AppCompatActivity() {
-    val db by lazy { NoteDB (this) }
+    val db by lazy { NoteDB(this) }
     private var noteId = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
         setupView()
         setupLstener()
-        noteId = intent.getIntExtra("intent_id",0)
-        Toast.makeText(this,noteId.toString(), Toast.LENGTH_SHORT).show()
     }
 
-    fun setupView(){
-        val intentType = intent.getIntExtra("intent_type",0)
-        when(intentType){
-            Constant.TYPE_CREATE ->{
+    fun setupView() {
+        val intentType = intent.getIntExtra("intent_type", 0)
+        when (intentType) {
+            Constant.TYPE_CREATE -> {
 
             }
-            Constant.TYPE_READ ->{
+            Constant.TYPE_READ -> {
                 button_save.visibility = View.GONE
+                getNote()
             }
         }
     }
 
     private fun setupLstener() {
-         button_save.setOnClickListener{
-             CoroutineScope(Dispatchers.IO).launch {
-                 db.noteDao().addNote(
-                     //addNote yg kita butuhkan adalah id,title,dan notenya
-                     //id disini karena sudah auto generate maka tidak harus diisi
-                     Note(0,edit_nama.text.toString(),edit_address1.text.toString(),edit_address2.text.toString())
-                 )
-                 finish()
-             }
-         }
+        button_save.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                db.noteDao().addNote(
+                    //addNote yg kita butuhkan adalah id,title,dan notenya
+                    //id disini karena sudah auto generate maka tidak harus diisi
+                    Note(
+                        0,
+                        edit_nama.text.toString(),
+                        edit_address1.text.toString(),
+                        edit_address2.text.toString()
+                    )
+                )
+                finish()
+            }
+        }
+    }
+
+    fun getNote() {
+        noteId = intent.getIntExtra("intent_id", 0)
+        CoroutineScope(Dispatchers.IO).launch {
+            val notes = db.noteDao().getNote(noteId)[0]
+            edit_nama.setText(notes.nama)
+            edit_address1.setText(notes.address1)
+            edit_address2.setText(notes.address2)
+        }
     }
 }
