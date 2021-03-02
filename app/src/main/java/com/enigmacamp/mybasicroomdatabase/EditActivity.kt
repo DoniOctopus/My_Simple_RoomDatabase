a8e45c25de9ab5e7612164ee95c1f6e9c58b0a0b
doni <donioctavian1016@gmail.com>
2021-03-03 00:43:18 +0700

    show_list_byid

--- app/src/main/java/com/enigmacamp/mybasicroomdatabase/EditActivity.kt
+++ app/src/main/java/com/enigmacamp/mybasicroomdatabase/EditActivity.kt
@@ -2,6 +2,9 @@ package com.enigmacamp.mybasicroomdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.enigmacamp.mybasicroomdatabase.room.Constant
 import com.enigmacamp.mybasicroomdatabase.room.Note
 import com.enigmacamp.mybasicroomdatabase.room.NoteDB
 import kotlinx.android.synthetic.main.activity_edit.*
@@ -12,10 +15,26 @@ import kotlinx.coroutines.launch
 
 class EditActivity : AppCompatActivity() {
     val db by lazy { NoteDB (this) }
+    private var noteId = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+        setupView()
         setupLstener()
+        noteId = intent.getIntExtra("intent_id",0)
+        Toast.makeText(this,noteId.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    fun setupView(){
+        val intentType = intent.getIntExtra("intent_type",0)
+        when(intentType){
+            Constant.TYPE_CREATE ->{
+
+            }
+            Constant.TYPE_READ ->{
+                button_save.visibility = View.GONE
+            }
+        }
     }
 
     private fun setupLstener() {
@@ -24,7 +43,7 @@ class EditActivity : AppCompatActivity() {
                  db.noteDao().addNote(
                      //addNote yg kita butuhkan adalah id,title,dan notenya
                      //id disini karena sudah auto generate maka tidak harus diisi
-                     Note(0,edit_title.text.toString(),edit_note.text.toString())
+                     Note(0,edit_nama.text.toString(),edit_address1.text.toString(),edit_address2.text.toString())
                  )
                  finish()
              }
--- app/src/main/java/com/enigmacamp/mybasicroomdatabase/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/mybasicroomdatabase/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.enigmacamp.mybasicroomdatabase.room.Constant
 import com.enigmacamp.mybasicroomdatabase.room.Note
 import com.enigmacamp.mybasicroomdatabase.room.NoteDB
 import kotlinx.android.synthetic.main.activity_edit.*
@@ -44,15 +45,19 @@ class MainActivity : AppCompatActivity() {
     //Berpindah ke EditActivity
     private fun setupListener(){
         button_create.setOnClickListener {
-            startActivity(Intent(this,EditActivity::class.java))
-//            intentEdit(Constant.TYPE_CREATE, 0)
+//            startActivity(Intent(this,EditActivity::class.java))
+            intentEdit(0,Constant.TYPE_CREATE)
         }
     }
 
     private fun setupRecyclerView() {
         noteAdapter = NoteAdapter(arrayListOf(),object : NoteAdapter.OnAdapterListener{
             override fun onClick(note: Note) {
-                Toast.makeText(applicationContext,note.title,Toast.LENGTH_SHORT).show()
+//                startActivity(Intent(
+//                    applicationContext,EditActivity::class.java)
+//                    .putExtra("intent_id", note.id)
+//                )
+                intentEdit(note.id,Constant.TYPE_READ)
             }
 
         })
@@ -62,5 +67,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun intentEdit(noteId : Int, intentType : Int){
+        startActivity(Intent(
+            applicationContext,EditActivity::class.java)
+            .putExtra("intent_id", noteId)
+            .putExtra("intent_type", intentType)
 
+        )
+    }
 }
